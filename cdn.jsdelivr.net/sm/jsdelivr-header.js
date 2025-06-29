dee46e3cb208ce63d8027830ac1d3686df18f851
chouaib2001 <chouaibdahbi6@gmail.com>
2025-06-29 20:24:27 +0100

    Add files via upload

---
+++ cdn.jsdelivr.net/sm/jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.39.0.
+ * Original file: /npm/@tailwindcss/browser@4.1.11/dist/index.global.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
